3b86bc156e1b2d045001bc4ec5667bed26db8870
youpy <youpy@buycheapviagraonlinenow.com>
2012-03-11 12:03:43 +0900

    add feed for favorites

--- server.rb
+++ server.rb
@@ -27,7 +27,10 @@ get '/welcome' do
   end
 
   SoundCloud::User.create!(:id_md5 => id_md5, :access_token_key => access_token_key, :access_token_secret => access_token_secret)
-  redirect to('/activities/%s.xml' % id_md5)
+
+  @id_md5 = id_md5
+
+  haml :welcome
 end
 
 get '/activities/:id.xml' do |id_md5|
@@ -67,6 +70,47 @@ get '/activities/:id.xml' do |id_md5|
                 xml.author origin['user']['username']
                 xml.itunes :author, origin['user']['username']
                 xml.itunes :summary, origin['description']
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+get '/activities/favorites/:id.xml' do |id_md5|
+  user = SoundCloud::User.where(:id_md5 => id_md5).first
+  access_token = OAuth::AccessToken.new(oauth_consumer, user.access_token_key, user.access_token_secret)
+  data = JSON.parse(access_token.get('https://api.soundcloud.com/me/activities/all.json').body)
+  me = JSON.parse(access_token.get('https://api.soundcloud.com/me.json').body)
+
+  builder do |xml|
+    xml.instruct! :xml, :version => '1.0'
+    xml.rss :version => "2.0", 'xmlns:itunes' => 'http://www.itunes.com/dtds/podcast-1.0.dtd' do
+      xml.channel do
+        xml.title "SoundCloud.com: Dashboard Favorite Podcast for %s" % me['username']
+        xml.description "SoundCloud.com: Dashboard Favorite Podcast for %s" % me['username']
+        xml.link url('/activities/favorites/%s.xml' % id_md5)
+
+        data['collection'].each do |activity|
+          if activity['type'] == 'favoriting'
+            origin = activity['origin']['track']
+            enclosure_url = origin['stream_url']
+            format = 'mp3'
+            username = origin['permalink_url'].match(/http:\/\/soundcloud\.com\/([^\/]+)/)[1]
+
+            if enclosure_url
+              enclosure_url += '?consumer_key=hE1HLJfuvbBHU3fX2S56w'
+
+              xml.item do
+                xml.title origin['title']
+                xml.description origin['title']
+                xml.link origin['permalink_url']
+                xml.guid origin['permalink_url']
+                xml.enclosure :url => 'http://youpy.no.de/soundcloud/download.%s?download_url=%s' % [format, CGI.escape(enclosure_url.sub(/^https/, 'http'))]
+                xml.author username
+                xml.itunes :author, username
               end
             end
           end
